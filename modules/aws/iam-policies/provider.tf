a53d6aa93efbcc56847f0fc3126cb12b2570a271
Manuel Garcia Sanes (MAC) <mgarcias@cisco.com>
2023-04-04 14:10:01 +0200

    owner

--- modules/aws/iam-policies/provider.tf
+++ modules/aws/iam-policies/provider.tf
@@ -11,7 +11,7 @@ terraform {
 }
 
 provider "github" {
+  owner = "cisco-dcloud" # or GITHUB_OWNER this is the organization (or owner of the repo)
   #token = var.policy_github_token # or `GITHUB_TOKEN`
   base_url = "https://github.com/" # or GITHUB_BASE_URL ex: https://terraformtesting-ghe.westus.cloudapp.azure.com/
-  owner = "cisco-dcloud" # or GITHUB_OWNER this is the organization (or owner of the repo)
 }
