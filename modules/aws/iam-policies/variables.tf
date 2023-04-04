6fb472c71aef2ee177f0def05ce45f337b5f8cdd
Manuel Garcia Sanes (MAC) <mgarcias@cisco.com>
2023-04-04 16:00:16 +0200

    token

--- modules/aws/iam-policies/variables.tf
+++ modules/aws/iam-policies/variables.tf
@@ -12,10 +12,10 @@ variable "policies" {
 #GITHUB
 ########
 
-# variable "policy_github_token" {
-#   type        = string
-#   description = "Github token. Can also be provided using env GITHUB_TOKEN "
-# }
+variable "policy_github_token" {
+  type        = string
+  description = "Github token. Can also be provided using env GITHUB_TOKEN "
+}
 
 variable "policy_github_branch" {
   type        = string
