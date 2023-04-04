f03118223c589346255447c755bc13aaa421bfa2
Manuel Garcia Sanes (MAC) <mgarcias@cisco.com>
2023-04-04 16:50:05 +0200

    fix

--- modules/aws/iam-policies/provider.tf
+++ modules/aws/iam-policies/provider.tf
@@ -12,7 +12,7 @@ terraform {
 
 provider "github" {
   owner = "cisco-dcloud" # or GITHUB_OWNER this is the organization (or owner of the repo)
-  token = policy_github_token #var.policy_github_token # or `GITHUB_TOKEN`
+  token = var.policy_github_token #var.policy_github_token # or `GITHUB_TOKEN`
   base_url = "https://github.com/" # or GITHUB_BASE_URL ex: https://terraformtesting-ghe.westus.cloudapp.azure.com/
   
 }
