37fe1cb5922188f182ca88cea906b5a5c8cbaa7c
Manuel Garcia Sanes (MAC) <mgarcias@cisco.com>
2023-04-05 11:13:38 +0200

    fix

--- modules/aws/iam-saml/outputs.tf
+++ modules/aws/iam-saml/outputs.tf
@@ -1,5 +1,5 @@
 output "all_saml_roles" {
-  value= { for i in range(length(var.iamUsers)) : "role_${i}" => {
+  value= { for i in range(length(var.samlRoles)) : "role_${i}" => {
       name = aws_iam_role.all_saml_roles[i].name,
       arn = aws_iam_role.all_saml_roles[i].arn,
     } 
