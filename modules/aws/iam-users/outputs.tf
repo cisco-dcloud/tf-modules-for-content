394e0d0867cdba96220a0ddb2c8d8e935452a311
Manuel Garcia Sanes (MAC) <mgarcias@cisco.com>
2023-04-05 09:58:46 +0200

    all_users

--- modules/aws/iam-users/outputs.tf
+++ modules/aws/iam-users/outputs.tf
@@ -11,3 +11,30 @@ output "users_arn" {
   value = aws_iam_user.all_users.*.arn
 }
 
+output "all_users" {
+  value= { for i in range(count(var.iamUsers)) : "user_${i}" => {
+      name = aws_iam_user.all_users[i].name,
+      arn = aws_iam_user.all_users[i].arn,
+      key = aws_iam_access_key.all_users[i].id
+      secret = aws_iam_access_key.all_users[i].secret
+    } 
+  }
+}
+
+
+
+
+# {
+#   user1 = {
+#   "name"=userName1
+#   "arn"= useArn1
+#   "key"= iserKey1
+#   "secret" = userSecret1
+#   },
+#   user2 = {
+#   "name"=userName2
+#   "arn"= useArn2
+#   "key"= iserKey2
+#   "secret" = userSecret2
+#   },
+# }
